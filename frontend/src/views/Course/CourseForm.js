71ee13053a61da01b994a73262f7dad7d2460451
Shashi <it21334238@my.sliit.lk>
2024-05-09 00:04:03 +0530

    Updated CSS in CourseForm.js and added Validations

--- frontend/src/views/Course/CourseForm.js
+++ frontend/src/views/Course/CourseForm.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
+import "./CourseForm.css"; // Import CSS file for styling
 
 const CourseForm = () => {
   const [name, setName] = useState("");
@@ -20,6 +21,11 @@ const CourseForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (!name || !description || !price || !file) {
+      setError("Please fill in all required fields.");
+      return;
+    }
+
     const formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", "v3cvsxfw");
@@ -31,14 +37,12 @@ const CourseForm = () => {
       );
 
       if (cloudResponse.status === 200) {
-        console.log(cloudResponse);
         setFileId(cloudResponse.data.asset_id);
         setFileUrl(cloudResponse.data.secure_url);
 
         try {
           const user = JSON.parse(localStorage.getItem("user"));
           const insId = user._id;
-          console.log(insId);
 
           const imageInfo = {
             public_id: cloudResponse.data.asset_id,
@@ -52,21 +56,18 @@ const CourseForm = () => {
             price: price,
             file: imageInfo,
           };
-          console.log(newCourse);
+
           const response = await axios.post(
             "http://localhost:4000/api/courseService/courses/newCourse",
             newCourse
           );
 
           if (response.status === 201) {
-            console.log(response);
             navigate("/allcourses");
           }
         } catch (error) {
           console.error(error);
         }
-      } else {
-        console.log(cloudResponse);
       }
     } catch (error) {
       console.error(error);
@@ -74,59 +75,53 @@ const CourseForm = () => {
   };
 
   return (
-    <div className="max-w-[700px] mx-auto ">
-      <form className="create" onSubmit={handleSubmit}>
-        <h3 className="text-[2rem] px-4">Create a New Course</h3>
-        <div className="items-center mb-4 pt-[30px] px-4">
-          <label className="mr-2 text-[1.5rem]">Course Name :</label>
+    <div className="page-container mt-10">
+      <form className="form-container" onSubmit={handleSubmit}>
+        <h3>Create a New Course</h3>
+        <div className="form-group">
+          <label>Course Name :</label>
           <input
             type="text"
             onChange={(e) => setName(e.target.value)}
             value={name}
-            className="bg-gray-100 w-full py-2 px-4 rounded-md focus:outline-none"
           />
         </div>
-        <div className="items-center mb-4 pt-[20px] px-4">
-          <lable className="mr-2 text-[1.5rem]">Price :</lable>
+        <div className="form-group">
+          <label>Price :</label>
           <input
             type="number"
             onChange={(e) => setPrice(e.target.value)}
             value={price}
-            // className={emptyFields.includes("price") ? "error" : ""}
-            className="bg-gray-100 w-full py-2 px-4 rounded-md focus:outline-none"
           />
         </div>
-        <div className="items-center mb-4 pt-[20px] px-4">
-          <lable className="mr-2 text-[1.5rem]">Description :</lable>
-          <br />
+        <div className="form-group">
+          <label>Description :</label>
           <textarea
             name="description"
             type="text"
             onChange={(e) => setDescription(e.target.value)}
             value={description}
-            // className={emptyFields.includes("description") ? "error" : ""}
-            // style={{ width: "379px", height: "100px", marginBottom: "10px" }}
-            className="bg-gray-100 w-full py-2 px-4 rounded-md focus:outline-none h-[100px]"
           />
         </div>
-        <div className="items-center mb-4 pt-[20px] px-4">
-          <label className="mr-4 text-[1.5rem]">Thumbnail : </label>
+        <div className="form-group">
+          <label>Thumbnail :</label>
           <input
             type="file"
-            id="myfile"
-            onChange={(e) => {
-              setFile(e.target.files[0]);
-            }}
-            className={emptyFields.includes("file") ? "error" : ""}
-            accept="image/*" //only accept image files
+            onChange={(e) => setFile(e.target.files[0])}
+            accept="image/*"
             name="filename"
           />
+          {file && (
+            <video
+              src={URL.createObjectURL(file)}
+              width="320"
+              height="240"
+              controls
+            />
+          )}
         </div>
-        <div className="items-center mb-4 mt-[30px] px-4">
-          <button
-            // className="TrainingButton"
-            className="bg-blue-500 hover:bg-blue-700 text-[1.2rem] text-white font-bold py-2 px-4 rounded"
-          >
+        <div className="form-group">
+          <button className="form-button" type="submit">
             Create Course
           </button>
           {error && <div className="error">{error}</div>}
