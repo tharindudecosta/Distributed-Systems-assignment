342e0d21307bfcfc77f4a9a5b05e5e9b2326882f
Tharindu De Costa <you@example.com>
2024-05-13 11:34:57 +0530

    navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
                       >
                         All Courses
                       </a>
-                      {role == "instuctor" ? (
+                      {role == "instructor" ? (
                         <a
                           href="/inscourse"
                           className="block px-4 py-2 text-sm text-white hover:bg-gray-700"
